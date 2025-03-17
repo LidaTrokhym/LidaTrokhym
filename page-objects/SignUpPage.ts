68e71e29aab2055e137f586194ed6bee5665d9ec
Lidiia Trokhym <ltrokh@softserveinc.com>
2025-03-17 11:26:16 +0200

    signUpPage

--- page-objects/SignUpPage.ts
+++ page-objects/SignUpPage.ts
@@ -9,7 +9,10 @@ export class SignUpPage {
     readonly userPassword: Locator;
     readonly confirmPassword: Locator;
     readonly registerButton: Locator;
-  
+    readonly roleDropdown: Locator;
+    readonly objectiveDropdown: Locator;
+    readonly startButton: Locator;
+
     constructor(page: Page) {
 
         this.page = page;
@@ -19,15 +22,28 @@ export class SignUpPage {
         this.userEmail = page.locator('#new_user_email');
         this.userPassword = page.locator('#new_user_password');
         this.registerButton = page.locator("[data-qa-selector='new_user_register_button']");
-   
+        this.roleDropdown = page.locator('#user_role');
+        this.objectiveDropdown = page.locator('#user_registration_objective');
+        this.startButton = page.locator("button[name='button']");
     }
 
     async signUp(firstname: string, lastname: string, username: string, useremail:string, userpassword: string) {
-        await this.firstName.fill(firstname)
-        await this.lastName.fill(lastname)
-        await this.userName.fill(username)
-        await this.userEmail.fill(useremail)
-        await this.userPassword.fill(userpassword)
-        await this.registerButton.click()
+        await this.firstName.fill(firstname);
+        await this.lastName.fill(lastname);
+        await this.userName.fill(username);
+        await this.userEmail.fill(useremail);
+        await this.userPassword.fill(userpassword);
+        await this.registerButton.click();
     }
+
+    async goto () {
+        await this.page.goto ('https://gitlab.testautomate.users');
+    }
+
+    async selectRoleandObjectiveandButton(role, object, button) {
+        await this.roleDropdown.selectOption({ value: role });
+        await this.objectiveDropdown.selectOption({ value: object });
+        await this.startButton.click(button);
+    }
+    
 }
--- tests/signup.spec.ts
+++ tests/signup.spec.ts
@@ -5,7 +5,9 @@ test('Sign Up', async ({ page }) => {
     const signUpPage = new SignUpPage(page);
 
 await page.goto('https://gitlab.testautomate.me/users/sign_up');
-await signUpPage.signUp('Lida', 'Trokhym', 'lida_test100', 'lidatrokhym4@gmail.com', '11111111Lida!'); 
-await expect(page).toHaveURL('https://gitlab.testautomate.me/users');
+await signUpPage.signUp('Lid7', 'Tro7', 'lida_test7', 'lidatrokhym7@gmail.com', '11111111Lida!');
+await signUpPage.selectRoleandObjectiveandButton('other', 'basics', 'Get started!');
+await expect(page).toHaveURL('https://gitlab.testautomate.me/dashboard/projects');
+}
 
-})
+)
